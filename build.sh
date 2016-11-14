6a5ee5fdd78f1fa17681d6fa9110694daf79771d
Jon Simpson <jon@jonsimpson.ca>
2016-11-13 22:11:57 -0500

    have the build use the current directory

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-mkdir _build
-
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build honours-project.tex
-bibtex _build/honours-project.aux
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build honours-project.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build honours-project.tex
+pdflatex -interaction=nonstopmode -halt-on-error honours-project.tex
+bibtex honours-project.aux
+pdflatex -interaction=nonstopmode -halt-on-error honours-project.tex
+pdflatex -interaction=nonstopmode -halt-on-error honours-project.tex
